356db2f5e1f4580943d4b84cb759ea85dbf85607
anna0lbi05 <anna.co@puccampinas.edu.br>
2026-06-09 18:26:38 -0300

    Implementação do algoritmo Merge Sort

--- experimento.py
+++ experimento.py
@@ -37,4 +37,59 @@ ordenado, trocas = bubble_sort(vetor_teste.copy())
 
 print("\nTeste Bubble Sort")
 print("Vetor ordenado:", ordenado)
-print("Quantidade de trocas:", trocas)    
+print("Quantidade de trocas:", trocas)  
+
+#Implementando o Merge Sort
+def merge_sort(vetor):
+    movimentacoes = [0]
+
+    def merge_sort_rec(lista):
+        if len(lista) <= 1:
+            return lista
+
+        meio = len(lista) // 2
+
+        esquerda = merge_sort_rec(lista[:meio])
+        direita = merge_sort_rec(lista[meio:])
+
+        return merge(esquerda, direita)
+
+    def merge(esquerda, direita):
+        resultado = []
+
+        i = 0
+        j = 0
+
+        while i < len(esquerda) and j < len(direita):
+
+            if esquerda[i] < direita[j]:
+                resultado.append(esquerda[i])
+                i += 1
+            else:
+                resultado.append(direita[j])
+                j += 1
+
+            movimentacoes[0] += 1
+
+        while i < len(esquerda):
+            resultado.append(esquerda[i])
+            i += 1
+            movimentacoes[0] += 1
+
+        while j < len(direita):
+            resultado.append(direita[j])
+            j += 1
+            movimentacoes[0] += 1
+
+        return resultado
+
+    vetor_ordenado = merge_sort_rec(vetor)
+
+    return vetor_ordenado, movimentacoes[0]
+
+# Teste do Merge Sort
+ordenado_merge, mov_merge = merge_sort(vetor_teste.copy())
+
+print("\nTeste Merge Sort")
+print("Vetor ordenado:", ordenado_merge)
+print("Quantidade de movimentações:", mov_merge)
